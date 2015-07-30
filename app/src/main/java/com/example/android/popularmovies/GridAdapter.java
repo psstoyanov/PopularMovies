d0acdb96351a1ae3467778872b7d0f125f60188a
Raz3r <kokotekilata@gmail.com>
2015-07-30 01:29:10 +0100

    Added simple onClick listener for the recyclerView. Added internet permissions and basic test with loading an image with Picasso.
    No errors when offline.

--- app/src/main/java/com/example/android/popularmovies/GridAdapter.java
+++ app/src/main/java/com/example/android/popularmovies/GridAdapter.java
@@ -1,5 +1,6 @@
 package com.example.android.popularmovies;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -8,6 +9,8 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -53,6 +56,11 @@ public class GridAdapter extends RecyclerView.Adapter<GridAdapter.ViewHolder>
         viewHolder.tvtitles.setText(movielist.getmName());
         viewHolder.imgThumbnail.setImageResource(movielist.getThumbnail());
 
+        //Use Picasso to load an image.
+        Picasso.with(viewHolder.imgThumbnail.getContext()).cancelRequest(viewHolder.imgThumbnail);
+        Picasso.with(viewHolder.imgThumbnail.getContext()).load("http://img13.deviantart.net/e8e8/i/2012/045/2/0/passage_wallpaper_by_trenchmaker-d4pp3zd.jpg").into(viewHolder.imgThumbnail);
+        //Picasso.with(viewHolder.imgThumbnail.getContext()).load(R.drawable.grid_item_mock).into(viewHolder.imgThumbnail);
+
     }
 
     @Override
@@ -68,17 +76,22 @@ public class GridAdapter extends RecyclerView.Adapter<GridAdapter.ViewHolder>
         public ImageView imgThumbnail;
         public TextView tvtitles;
 
-        public ViewHolder(View itemView)
+        public ViewHolder(final View itemView)
         {
+
             super(itemView);
+
             imgThumbnail = (ImageView) itemView.findViewById(R.id.img_thumbnail);
             tvtitles = (TextView) itemView.findViewById(R.id.tv_movie_title);
 
-            /* TODO onClickListener for separate items from the adapter.*/
+            /* TODO onClickListener for separate items from the adapter. Use: http://antonioleiva.com/recyclerview/*/
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
-                public void onClick(View v) {
+                public void onClick(View v)
+                {
                     Log.d(TAG, "Element " + getAdapterPosition() + " clicked.");
+
+                    Toast.makeText(itemView.getContext(),"Recycler View" + getAdapterPosition() + " clicked",Toast.LENGTH_SHORT).show();
                 }
             });
         }
