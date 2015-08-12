8724af5642458e25c25d07a1c9d5f1f8c22003c0
Raz3r <kokotekilata@gmail.com>
2015-08-12 01:54:17 +0100

    Both tables are tested and successfully created.

--- app/src/androidTest/java/com/example/android/popularmovies/data/TestDb.java
+++ app/src/androidTest/java/com/example/android/popularmovies/data/TestDb.java
@@ -108,6 +108,82 @@ public class TestDb extends AndroidTestCase {
         also make use of the ValidateCurrentRecord function from within TestUtilities.
     */
     public void testSortOrderTable() {
+        insertSortOrder();
+
+    }
+
+    /*
+        Students:  Here is where you will build code to test that we can insert and query the
+        database.  We've done a lot of work for you.  You'll want to look in TestUtilities
+        where you can use the "createMoviesValues" function.  You can
+        also make use of the validateCurrentRecord function from within TestUtilities.
+     */
+    public void testMoviesTable() {
+        // First insert the sort order, and then use the sortorderRowId to insert
+        // the movies. Make sure to cover as many failure cases as you can.
+
+
+        // Instead of rewriting all of the code we've already written in testSortOrderTable
+        // we can move this code to insertSortOrder and then call insertSortOrder from both
+        // tests. Why move it? We need the code to return the ID of the inserted location
+        // and our testSortOrderTable can only return void because it's a test.
+
+        long sortorderRowId = insertSortOrder();
+        // Make sure we have a valid row ID.
+        assertFalse("Error: Sort Order Not Inserted Correctly", sortorderRowId == -1L);
+
+        // First step: Get reference to writable database
+        MoviesDBHelper dbHelper = new MoviesDBHelper(mContext);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        // Second step (Movies): Create ContentValues of what you want to insert
+        // (you can use the createMoviesValues TestUtilities function if you wish)
+        ContentValues moviesValues = TestUtilities.createMoviesValues(sortorderRowId);
+
+        // Third step (Movies): Insert ContentValues into database and get a row ID back
+        long moviesRowId = db.insert(MoviesContract.MoviesEntry.TABLE_NAME, null, moviesValues);
+        assertTrue( moviesRowId != -1);
+
+        // Fourth step (Movies): Query the database and receive a Cursor back
+        // A cursor is your primary interface to the query results.
+        Cursor movieCursor = db.query(
+                MoviesContract.MoviesEntry.TABLE_NAME, // Table to Query
+                null, // leaving "columns" null just returns all the columns.
+                null, // Columns for the "where" clause
+                null, // Values for the "where" clause
+                null, // columns to group by
+                null, // column to filter by row groups
+                null // sort order
+        );
+
+        // Move the cursor to a valid database row
+        assertTrue( " Error: No Records returned from movie query", movieCursor.moveToFirst());
+
+        // Fifth step (Movie): Validate data in resulting Cursor with the original ContentValues
+        // (you can use the validateCurrentRecord function in TestUtilities to validate the
+        // query if you like)
+        TestUtilities.validateCurrentRecord("testInsertReadDb movieEntry failed to validate",
+                movieCursor, moviesValues);
+
+        // Finally, close the cursor and database
+        /*assertFalse( " Error: More than one record returned from movie query",
+                movieCursor.moveToNext());*/
+
+        movieCursor.close();
+        dbHelper.close();
+
+    }
+
+
+    /*
+        Students: This is a helper method for the testMoviesTable quiz. You can move your
+        code from testSortOrderTable to here so that you can call this code from both
+        testMoviesTable and testSortOrderTable.
+     */
+    public long insertSortOrder() {
+
+
+
         // First step: Get reference to writable database
         // If there's an error in those massive SQL table creation Settings,
         // errors will be thrown here when you try to get a writable database.
@@ -157,51 +233,6 @@ public class TestDb extends AndroidTestCase {
         // Sixth step: Close the cursor and database.
         cursor.close();
         db.close();
-
-
-
-    }
-
-    /*
-        Students:  Here is where you will build code to test that we can insert and query the
-        database.  We've done a lot of work for you.  You'll want to look in TestUtilities
-        where you can use the "createMoviesValues" function.  You can
-        also make use of the validateCurrentRecord function from within TestUtilities.
-     */
-    public void testWeatherTable() {
-        // First insert the location, and then use the locationRowId to insert
-        // the weather. Make sure to cover as many failure cases as you can.
-
-        // Instead of rewriting all of the code we've already written in testSortOrderTable
-        // we can move this code to insertLocation and then call insertLocation from both
-        // tests. Why move it? We need the code to return the ID of the inserted location
-        // and our testSortOrderTable can only return void because it's a test.
-
-        // First step: Get reference to writable database
-
-        // Create ContentValues of what you want to insert
-        // (you can use the createMoviesValues TestUtilities function if you wish)
-
-        // Insert ContentValues into database and get a row ID back
-
-        // Query the database and receive a Cursor back
-
-        // Move the cursor to a valid database row
-
-        // Validate data in resulting Cursor with the original ContentValues
-        // (you can use the validateCurrentRecord function in TestUtilities to validate the
-        // query if you like)
-
-        // Finally, close the cursor and database
-    }
-
-
-    /*
-        Students: This is a helper method for the testWeatherTable quiz. You can move your
-        code from testSortOrderTable to here so that you can call this code from both
-        testWeatherTable and testSortOrderTable.
-     */
-    public long insertLocation() {
-        return -1L;
+        return sortorderRowId;
     }
 }
